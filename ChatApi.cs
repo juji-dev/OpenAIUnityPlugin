ffdff4b442b1f4adc25b41633bf8f06795bcefa4
juji-dev <joey.r.breslin@gmail.com>
2024-04-12 17:19:24 -0700

    Added source files.

---
+++ ChatApi.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace OpenAi
+{
+    public class ChatApi
+    { 
+        public delegate void OpenAiResponseEventHandler(string message);
+        public static event OpenAiResponseEventHandler? OnOpenAiResponseReceived;
+
+        private static readonly string apiEndPoint = "https://api.openai.com/v1/chat/completions";
+        private static readonly string openAiModel = "gpt-3.5-turbo";
+        private static readonly string apiKey = "YOUR_OPEN_AI_API_KEY";
+        private static readonly string contentType = "application/json";
+        private static readonly string authorizationPrefix = "Bearer ";
+
+        public static IEnumerator PostOpenAiRequest(Message message)
+        {
+            string requestBodyJsonStr = "{\"model\": \"" + openAiModel +
+                                        "\", \"messages\": [{\"role\": \"" +
+                                        message.role + "\", \"content\": \"" +
+                                        message.content + "\"}]}";
+
+            UnityWebRequest request = new UnityWebRequest(apiEndPoint, "POST");
+
+            request.SetRequestHeader("Content-Type", contentType);
+            request.SetRequestHeader("Authorization", authorizationPrefix + apiKey);
+
+            byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(requestBodyJsonStr);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error: " + request.error);
+            }
+            else
+            {
+                ChatCompletionResponse responseMessage = JsonUtility.FromJson<ChatCompletionResponse>(request.downloadHandler.text);
+                OnOpenAiResponseReceived?.Invoke(responseMessage.choices[0].message.content);
+            }
+        }
+    }
+}
---
+++ Types.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OpenAi
+{
+    [Serializable]
+    public struct ChatCompletionResponse
+    {
+        public string id;
+        public string _object;
+        public long created;
+        public string model;
+        public Choice[] choices;
+        public Usage usage;
+        public string system_fingerprint;
+    }
+
+    [Serializable]
+    public struct Choice
+    {
+        public int index;
+        public Message message;
+        public object logprobs;
+        public string finish_reason;
+    }
+
+    [Serializable]
+    public struct Usage
+    {
+        public int prompt_tokens;
+        public int completion_tokens;
+        public int total_tokens;
+    }
+
+    [Serializable]
+    public struct Message
+    {
+        public string role;
+        public string content;
+
+        public Message(string role, string content)
+        {
+            this.role = role;
+            this.content = content;
+        }
+    }
+}
